e651409c6b6295027f191a47e02d6b637406f005
mimisull <101231644+mimisull@users.noreply.github.com>
2022-09-08 15:15:04 -0700

    Add files via upload

--- fintech_finder.py
+++ fintech_finder.py
@@ -79,7 +79,9 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 # @TODO:
 # From `crypto_wallet.py import the functions generate_account, get_balance,
 #  and send_transaction
-# YOUR CODE HERE
+from crypto_wallet import generate_account
+from crypto_wallet import get_balance
+from crypto_wallet import send_transaction
 
 ################################################################################
 # Fintech Finder Candidate Information
@@ -130,7 +132,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account=generate_account()
 
 ##########################################
 
@@ -146,7 +148,8 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+ether_balance = get_balance(w3, account.address)
+st.sidebar.write(ether_balance)
 
 ##########################################
 
